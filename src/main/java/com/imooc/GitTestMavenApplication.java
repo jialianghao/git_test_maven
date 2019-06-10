6b02d40cebab370250a21a082f556622b6119d6c
sunjialiang <sunjialiang@9fbank.com.cn>
2019-06-10 15:34:59 +0800

    将本地项目提交到远程仓库

---
+++ src/main/java/com/imooc/GitTestMavenApplication.java
@@ -0,0 +1,13 @@
+package com.imooc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTestMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTestMavenApplication.class, args);
+	}
+
+}
